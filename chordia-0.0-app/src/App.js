2429763a45fdf1f6f1cc5cefaf8f1f89e104fdd8
Codrexerino <jonathan.rensvik.viddal@gmail.com>
2024-03-19 14:20:04 +0100

    added header

--- chordia-0.0-app/src/App.js
+++ chordia-0.0-app/src/App.js
@@ -1,1 +1,13 @@
+import React from 'react';
+import './App.css';
+import Header from './Header'; // Import the Header component
 
+function App() {
+  return (
+    <div className="App">
+      <Header title="Chordia 0.0" /> {/* Use the Header component */}
+    </div>
+  );
+}
+
+export default App;
